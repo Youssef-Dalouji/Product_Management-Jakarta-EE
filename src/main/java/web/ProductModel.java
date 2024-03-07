dd6ba56b92b71698cf086c817b99b348aa495d66
Youssef Dalouji <daloujiy@gmail.com>
2024-03-07 23:55:00 +0000

    Config Model Product

--- src/main/java/web/ProductModel.java
+++ src/main/java/web/ProductModel.java
@@ -1,5 +1,35 @@
 package web;
 
-public class ProductModel {
+import java.io.Serializable;
+import java.util.List;
+import metier.entities.Product;
 
+public class ProductModel implements Serializable {
+	
+	private String key;
+	private List<Product> table;
+	
+	public ProductModel() {
+		super();
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public List<Product> getTable() {
+		return table;
+	}
+
+	public void setTable(List<Product> table) {
+		this.table = table;
+	}
+	
+	
+	
+	
 }
